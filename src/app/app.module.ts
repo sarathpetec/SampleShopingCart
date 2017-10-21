7040f0372434956137fa1b9980f60e2a02209285
Sarath J Nair <sarathjnair@gmail.com>
2017-10-22 01:05:52 +0530

    Sample Component Create.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {CourcesComponent} from './cources.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CourcesComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/cources.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'courses',
+  template: '<h2>Cources</h2>'
+})
+export class CourcesComponent {
+
+}
