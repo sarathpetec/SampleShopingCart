7f0805be19012b579c745981a40c92353916e3c8
Sarath J Nair <sarathjnair@gmail.com>
2017-10-22 11:00:12 +0530

    show Image via img tag

--- src/app/cources.component.ts
+++ src/app/cources.component.ts
@@ -8,12 +8,14 @@ import {CoursesService} from './courses.service';
     <ul>
       <li *ngFor="let course of courses">{{course}}</li>
     </ul>
+    <img src="{{imgUrl}}">
   `
 })
 export class CourcesComponent {
 
   tittle = 'Courses tittle';
   courses;
+  imgUrl = 'https://s1.postimg.org/37jme52re7/Koala.jpg';
 
   constructor(service: CoursesService) {
     this.courses = service.getCourses();
