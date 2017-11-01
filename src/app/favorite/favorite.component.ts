7658eb6c2718c74d5e64d615c3af11dc31e5fd81
Sarath J Nair <sarathjnair@gmail.com>
2017-11-01 22:51:13 +0530

    Output property added

--- src/app/favorite/favorite.component.ts
+++ src/app/favorite/favorite.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'favorite',
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class FavoriteComponent implements OnInit {
 
   @Input() isFavorite;
+  @Output() change = new EventEmitter();
 
   constructor() { }
 
@@ -17,6 +18,7 @@ export class FavoriteComponent implements OnInit {
   onClick() {
     console.log("OnClick method triggered")
     this.isFavorite.isSelected = !this.isFavorite.isSelected;
+    this.change.emit();
   }
 
 }
