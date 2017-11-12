fdc6a05c5be64b6ac5d5ca200031b92582c5e05b
Sarath J Nair <sarathjnair@gmail.com>
2017-11-12 16:56:21 +0530

    Expected exception catching.

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -2,6 +2,9 @@ import {Component, OnInit} from '@angular/core';
 import {Http} from '@angular/http';
 import {post} from 'selenium-webdriver/http';
 import {PostService} from '../services/post.service';
+import {AppError} from '../exceptions/app-error';
+import {NotFoundError} from '../exceptions/not-found-error';
+import {BadInputError} from '../exceptions/bad-input-error';
 
 @Component({
   selector: 'app-posts',
@@ -31,19 +34,29 @@ export class PostsComponent implements OnInit {
       .subscribe(response => {
         post.id = response.json().id;
         this.posts.splice(0, 0, post);
-      }, error => {
-        alert('An UnExpected Error Occurred.');
-        console.log('Error Occurred: ' + error);
+      }, (error: AppError) => {
+        if (error instanceof BadInputError) {
+          alert('Bad Request Exception');
+          console.log('Bad Request Exception Error Occurred: ' + error);
+        } else {
+          alert('An UnExpected Error Occurred.');
+          console.log('Error Occurred: ' + error);
+        }
       });
   }
 
   deletePost(inputPost: HTMLInputElement) {
-    this.service.deletePost(inputPost.id).subscribe(response => {
+    this.service.deletePost(356).subscribe(response => {
       const index = this.posts.indexOf(inputPost);
       this.posts.splice(index, 1);
-    }, error => {
-      alert('An UnExpected Error Occurred.');
-      console.log('Error Occurred: ' + error);
+    }, (error: AppError) => {
+      if (error instanceof NotFoundError) {
+        alert('This Post has already been deleted.');
+        console.log('This Post has already been deleted');
+      } else {
+        alert('An UnExpected Error Occurred.');
+        console.log('Error Occurred: ' + error);
+      }
     });
   }
 
--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,5 +1,11 @@
 import {Injectable} from '@angular/core';
 import {Http} from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
+import {Observable} from 'rxjs/Observable';
+import {AppError} from '../exceptions/app-error';
+import {NotFoundError} from '../exceptions/not-found-error';
+import {BadInputError} from '../exceptions/bad-input-error';
 
 @Injectable()
 export class PostService {
@@ -14,11 +20,21 @@ export class PostService {
   }
 
   createPost(inputPost) {
-    return this.http.post(this.url, JSON.stringify(inputPost));
+    return this.http.post(this.url, JSON.stringify(inputPost)).catch((error: Response) => {
+      if (error.status === 400) {
+        return Observable.throw(new BadInputError(error.json()));
+      }
+      return Observable.throw(new AppError(error));
+    });
   }
 
   deletePost(id) {
-    return this.http.delete(this.url + '/' + id);
+    return this.http.delete(this.url + '/' + id).catch((error: Response) => {
+      if (error.status === 404) {
+        return Observable.throw(new NotFoundError(error));
+      }
+      return Observable.throw(new AppError(error));
+    });
   }
 
 }
