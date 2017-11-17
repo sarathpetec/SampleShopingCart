dc4fb611e37d184dc7dd66f6db2387336519d877
Sarath <sarathjnair@gmail.com>
2017-11-17 20:19:48 +0530

    router updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -51,7 +51,7 @@ import { NotFoundComponent } from './not-found/not-found.component';
     RouterModule.forRoot([
       {path: '', component: HomeComponent},
       {path: 'followers', component: GithubFollowersComponent},
-      {path: 'profile/:username', component: GithubProfileComponent},
+      {path: 'followers/:username', component: GithubProfileComponent},
       {path: 'posts', component: PostsComponent},
       {path: '**', component: NotFoundComponent},
     ])
